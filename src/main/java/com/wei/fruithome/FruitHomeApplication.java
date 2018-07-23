b185e230cc098ea43adf42d5c55e41d53be9bf58
wangpf <wangpf@csdn.net>
2018-07-23 13:45:25 +0800

    init code

---
+++ src/main/java/com/wei/fruithome/FruitHomeApplication.java
@@ -0,0 +1,12 @@
+package com.wei.fruithome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruitHomeApplication.class, args);
+	}
+}
