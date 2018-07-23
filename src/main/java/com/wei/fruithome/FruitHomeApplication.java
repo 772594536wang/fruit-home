af9acc3a2fd0b4ebed0d9233defdc27dad7c85b6
wangpf <wangpf@csdn.net>
2018-07-23 14:21:25 +0800

    test

--- src/main/java/com/wei/fruithome/FruitHomeApplication.java
+++ src/main/java/com/wei/fruithome/FruitHomeApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FruitHomeApplication {
 
 	public static void main(String[] args) {
+        System.out.println("1111");
 		SpringApplication.run(FruitHomeApplication.class, args);
 	}
 }
