991eaf4abb0a64c103f7c859d4733dd146b5a487
nowayingswang <nowayings@gmail.com>
2018-07-24 00:02:44 +0800

    add common code

---
+++ src/main/java/com/wei/fruithome/dao/user/UserMapper.java
@@ -0,0 +1,30 @@
+package com.wei.fruithome.dao.user;
+
+import com.wei.fruithome.entity.user.User;
+import com.wei.fruithome.entity.user.UserExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface UserMapper {
+    long countByExample(UserExample example);
+
+    int deleteByExample(UserExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    List<User> selectByExample(UserExample example);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByExample(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
